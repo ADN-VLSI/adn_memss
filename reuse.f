cb70de76a790671e9c9016dd466ebb204f8727f7
Foez Ahmed <foez.official@gmail.com>
2026-09-17 14:51:07 +0600

    auto-update

--- reuse.f
+++ reuse.f
@@ -3,6 +3,7 @@ ${ADN_MEMSS}/interface/dummy_interface.sv
 ${ADN_MEMSS}/source/adn_memss_address_reservation_table.sv
 ${ADN_MEMSS}/source/adn_memss_address_reservation_unit.sv
 ${ADN_MEMSS}/source/adn_memss_alu.sv
+${ADN_MEMSS}/source/adn_memss_fsm.sv
 ${ADN_MEMSS}/source/dummy_rtl.sv
 -i ${ADN_COMMON}/include
 -i ${ADN_MEMSS}/include
