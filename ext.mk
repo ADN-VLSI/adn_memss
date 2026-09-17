10c770d1e972f33d78915118f3d4db0448ce8f17
Md. Mohiuddin Reyad <65802483+Reyad07@users.noreply.github.com>
2026-09-17 12:02:31 +0600

    Update environment variables in ext.mk

--- ext.mk
+++ ext.mk
@@ -1,7 +1,7 @@
 HAS_SUBMODULES = 1
 
-export ADN_TEMPLATE=$(CURDIR)
-export REPO_NAME_EXP=ADN_TEMPLATE
+export ADN_MEMSS=$(CURDIR)
+export REPO_NAME_EXP=ADN_MEMSS
 
 export ADN_COMMON=$(REPO_ROOT)/submodule/adn_common
 
